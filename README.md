6345a7d58802cef365b830dcde121060f93524bc
Dong Kyu Kim <dongkyukim00@gmail.com>
2021-02-11 19:00:54 -0600

    updated index.js, fixed remaining bugs in calculator

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ lifecycle management as needed.
 4. Focused on just creating a project with React first
 
 <ins>Known bugs:</ins>
-1. Zero will not appear before decimal point
+1. ~~Zero will not appear before decimal point~~
 2. ~~Spamming multiple operators in a row will result in error~~
 3. ~~Ignore operators that are entered before numbers~~
 4. ~~Display error message if dividing by zero~~
@@ -33,6 +33,19 @@ lifecycle management as needed.
  Fixed bugs #2-4
  Still need to fix bug #1
  New bug: Limit display character limit to prevent overflow
+
+ # Day 3:
+
+ Fixed remaining bugs:
+ Added leading 0 in front of decimal point if no other numbers
+ Removed trailing 0's
+ Included negative sign, decimal, and e sign into character count
+ Set maximum character limit to 9
+ Added comments
+ Applied DRY principle
+ Updated to allow for floating point calculations
+ 
+
 
 
 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -21,6 +21,12 @@ const operate = (x, y, str) => {
   }
 }
 
+// Character limit on calculator
+let lim = 9;
+
+// Number of leading zeros after decimal point if |number| < 1 (i.e. 0.0000134, there are 4 zeros after decimal)
+let leadZeros = 0;
+
 // Create buttons for calculator
 class CalculatorButtons extends React.Component {
   render() {
@@ -59,7 +65,7 @@ class Calculator extends React.Component {
       display: '0',
       arr: [],
       startNewNumber: false,
-      operatorClicked: false
+      operatorClicked: true
     }
     this.handleClick = this.handleClick.bind(this);
   }
@@ -74,32 +80,10 @@ class Calculator extends React.Component {
           display: '0',
           arr: [],
           startNewNumber: false,
-          operatorClicked: false
+          operatorClicked: true
         })
         break;
       case '0':
-        // If there is no display, add a 0 and return
-        if (this.state.display.toString() === '' || this.state.startNewNumber) {
-          this.setState({
-            display: '0',
-            operatorClicked: false
-          });
-        }
-        // Append numbers to string and remove leading zeros
-        if (!this.state.startNewNumber) {
-          this.setState(state => ({
-            display: [...state.display.toString(), val].join('').replace(/^0/,''),
-            operatorClicked: false
-          }))
-        // Start a new number if startNewNumber is true
-        } else {
-          this.setState({
-            display: [val].join('').replace(/^0/,''),
-            startNewNumber: false,
-            operatorClicked: false
-          })
-        }
-        break;
       case '1':
       case '2':
       case '3':
@@ -109,12 +93,35 @@ class Calculator extends React.Component {
       case '7':
       case '8':
       case '9':
+        // If there is no display, add a 0 and return
+        if ((this.state.display.toString() === '' || this.state.startNewNumber) && val === '0') {
+          this.setState({
+            display: '0',
+            operatorClicked: false
+          });
+          return;
+        }
         // Append numbers to string and remove leading zeros
         if (!this.state.startNewNumber) {
-          this.setState(state => ({
+          // If number of characters exceeds 10, do nothing
+          if (this.state.display.toString().length >= lim) {
+            return;
+          }
+          // If there is a 0 in front of a decimal point, don't replace the leading 0
+          let strArr = this.state.display.toString().split('.');
+          if (strArr[0] === '0' && strArr.length === 2) {
+            // Number of leading zeros after decimal
+            let tmp = strArr[1].match(/^0+/);
+            leadZeros = tmp ? tmp[0].length : 0;
+            this.setState(state => ({
+              display: [...state.display.toString(), val].join('')
+            }))
+          } else {
+            this.setState(state => ({
             display: [...state.display.toString(), val].join('').replace(/^0/,''),
             operatorClicked: false
-          }))
+            }))
+          }
         // Start a new number if startNewNumber is true
         } else {
           this.setState({
@@ -123,7 +130,7 @@ class Calculator extends React.Component {
             operatorClicked: false
           })
         }
-        // If there are no numbers in front of the decimal point, add a 0
+       
         break;
       case '/':
       case '*':
@@ -148,7 +155,10 @@ class Calculator extends React.Component {
           // Display error message and return if dividing by 0
           if (this.state.arr[2] === 0 && this.state.arr[1] === '/') {
             this.setState({
-              display: "Divided by 0"
+              display: "Divided by 0",
+              arr: [],
+              startNewNumber: true,
+              operatorClicked: true
             })
             return;
           }
@@ -157,7 +167,10 @@ class Calculator extends React.Component {
           if (typeof(this.state.arr[0]) !== 'number' || typeof(this.state.arr[1]) !== 'string' 
             || typeof(this.state.arr[2]) !== 'number' || typeof(this.state.arr[3]) !=='string') {
             this.setState({
-              display: "Error, AC"
+              display: "Error, AC",
+              arr: [],
+              startNewNumber: true,
+              operatorClicked: true
             })
             return;
           }
@@ -166,13 +179,23 @@ class Calculator extends React.Component {
           let result = operate(this.state.arr[0], this.state.arr[2], this.state.arr[1]);
 
           // Prevent character length from exceeding 10
-          if (result.toString().length > 10) {
-            result = Number.parseFloat(result).toPrecision(10);
+          let hardlim = lim;
+          if (leadZeros > 0) {
+            hardlim = hardlim - leadZeros;
+          }
+          if (result.toString().includes('-')) {
+            hardlim = hardlim - 1;
+          } 
+
+          if (result.toString().length > lim) {
+            result = Number.parseFloat(result).toPrecision(hardlim);
             if (result.toString().includes('e')) {
-              result = Number.parseFloat(result).toPrecision(7);
-              console.log('eeee');
+              result = Number.parseFloat(result).toPrecision(hardlim - 3);
             }
           }
+
+          // Remove trailing zeros from result
+          result = Number(result.toString().replace(/0$/,''));
 
           // Add result and next operator to array
           let newArr = [result, this.state.arr[3]];
@@ -195,22 +218,36 @@ class Calculator extends React.Component {
       
       // Toggle negative sign 
       case '+/-':
+        let regex = /^-/;
+        // No negative sign in front of just zero
         if (this.state.display.toString() === '0') {
           return;
-        } else if (!this.state.display.toString().includes('-')) {
+        } else if (!regex.test(this.state.display.toString())) {
           this.setState(state => ({
             display: ['-', ...state.display.toString()].join(''),
           }))
         } else {
           this.setState(state => ({
-            display: state.display.toString().slice(1),
+            display: state.display.toString().replace(/-?/,''),
           }))
         }
         break;
       
       // Divide by 100 
       case '%':
-        let result = Number(this.state.display / 100);
+        let result = Number(this.state.display) / 100;
+        // Prevent character length from exceeding 10
+        let hardlim = lim;
+        if (result.toString().includes('-')) {
+          hardlim = lim - 1;
+        } 
+
+        if (result.toString().length > lim) {
+          result = Number.parseFloat(result).toPrecision(hardlim);
+          if (result.toString().includes('e')) {
+            result = Number.parseFloat(result).toPrecision(hardlim - 3);
+          }
+        }
         this.setState({
           display: result
         })
